37d7bc1500c143c31c23e076febf1ea2e487d80d
Riki <riki@obsessedanalytics.com>
2016-07-26 12:35:05 +1200

    inbetween uploads

--- DecisionTree/Program.cs
+++ DecisionTree/Program.cs
@@ -13,27 +13,50 @@ namespace DecisionTree
         static void Main(string[] args)
         {
 
+            double secondPick = 0.0668;
+            double thirdPick = 0.92725;
+            double secondPickCount = 0;
+            double thirdPickCount = 0;
+            double n = 10;
+            Random rand = new Random();
 
-            // Define a function H that tells whether two points 
-            // are inside a unit circle (a circle of radius one):
-            // 
-            Func<double, double, double> H =
-                (x, y) => (Math.Pow(x,16.5) / (Math.Pow(x,16.5) + Math.Pow(y,16.5)) <= 1) ? 1 : 0;
+            for(int i = 0; i < n; i++)
+            {
+                /*
+                double rand1 = BoxMuller(rand);
+                if(rand1 < secondPick)
+                {
+                    secondPickCount++;
+                }
+                else if(rand1 > thirdPick)
+                { 
+                     thirdPickCount++;
+                }
+                */
+                Console.WriteLine(BoxMuller(rand));
+            }
 
-            // We will check how many points in the square (-1,-1), (-1,+1), 
-            // (+1, -1), (+1, +1) fall into the circle defined by function H.
-            // 
-            double[] from = { -1, -1 };
-            double[] to = { +1, +1 };
+            secondPickCount = secondPickCount / n;
+            thirdPickCount = thirdPickCount / n;
 
-            int samples = 100000;
-
-            // Integrate it! 
-            double area = MonteCarloIntegration.Integrate(x => H(x[0], x[1]), from, to, samples);
-
-            // Output should be approximately 3.14.
+            Console.WriteLine("Second pick probability: {0}", secondPickCount);
+            Console.WriteLine("Third pick probability: {0}", thirdPickCount);
+            Console.ReadLine();
 
 
+        }
+
+        static double BoxMuller(Random rand)
+        {
+            uint u = GetUint();
+            return (u + 1.0) * (1/(Math.Pow(2,32) + 2));
+        }
+
+        static uint GetUint()
+        {
+            uint m_z = 36969 * (m_z & 65535) + (m_z >> 16);
+            uint m_w = 18000 * (m_w & 65535) + (m_w >> 16);
+            return (m_z << 16) + m_w;
         }
     }
 }
